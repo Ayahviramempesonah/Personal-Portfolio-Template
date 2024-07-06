522a7e66887e4845ae646831a56c7ccd583bfbbd
bayusatriapurnomo <bayusatriapurnomo@gmail.com>
2024-07-06 21:22:45 +0000

    update js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,23 @@
 'use strict';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQLrsnr7LEmgUYk3TNlcFmi5TIZK_Bh4Y",
+  authDomain: "coder-tamvan-portofolio90.firebaseapp.com",
+  projectId: "coder-tamvan-portofolio90",
+  storageBucket: "coder-tamvan-portofolio90.appspot.com",
+  messagingSenderId: "292183057932",
+  appId: "1:292183057932:web:4a0aef20cb5fcf3ce3b029"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 
 
 // element toggle function
